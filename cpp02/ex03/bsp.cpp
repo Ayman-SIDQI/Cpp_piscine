580ea92453bfe5cc8ed43c2558e92156311028dc
Ayman Sidqi <90562697+Ayman-SIDQI@users.noreply.github.com>
2023-09-05 21:34:51 +0100

    "heheh"

--- cpp02/ex03/bsp.cpp
+++ cpp02/ex03/bsp.cpp
@@ -6,7 +6,7 @@
 /*   By: asidqi <asidqi@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/04 20:09:39 by asidqi            #+#    #+#             */
-/*   Updated: 2023/09/05 20:58:27 by asidqi           ###   ########.fr       */
+/*   Updated: 2023/09/05 21:34:42 by asidqi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,6 @@ bool	bsp( Point const a, Point const b, Point const c, Point const p)
 	w1 = ((a.gX() * (c.gY() - a.gY())) + ((p.gY() - a.gY()) * (c.gX() - a.gX())) - (p.gX() * (c.gY() - a.gY()))) 
 		/ (((b.gY() - a.gY()) * (c.gX() - a.gX())) - ((b.gX() - a.gX()) * (c.gY() - a.gY())));
 	w2 = (p.gY() - a.gY() - (w1 * (b.gY() - a.gY()))) / (c.gY() - a.gY());
-	
-	std::cout << w1 << std::endl;
-	std::cout << "W1	" << std::endl;
-	std::cout << w2 << std::endl;
-	std::cout << "W2" << std::endl;
-	
 	if (w1 > 0 && w2 > 0 && (w1 + w2) < 1)
 		return (true);
 	return (false);
